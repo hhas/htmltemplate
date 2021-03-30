7a1166697789c34347969c55d6ef752d150f9eaa
hhas <hhas@users.sourceforge.net>
2021-03-30 13:43:51 +0100

    fixed `html.unescape` import (previously used `HTMLParser.unescape`, which is removed in 3.9)

--- htmltemplate.py
+++ htmltemplate.py
@@ -1,6 +1,6 @@
 """ htmltemplate -- A simple, powerful HTML templating system for Python 3.
 
-Copyright (C) 2007-2020 HAS
+Copyright (C) 2007-2021 HAS
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -30,7 +30,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #
 
 
-import html.parser, keyword, re
+import html, html.parser, keyword, re
 
 __all__ = ['ParseError', 'Node', 'Template', 'encodeentity', 'decodeentity']
 
@@ -54,7 +54,7 @@ def encodeentity(s):
 	""" Default encoder for HTML entities: replaces &, <, > and " characters only. """
 	return s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;').replace('"', '&quot;')
 
-decodeentity = html.parser.HTMLParser().unescape
+decodeentity = html.unescape
 
 
 #####################################################################
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name = 'htmltemplate',
-	version = '2.2.0',
+	version = '2.2.1',
 	description = 'A simple, powerful [X]HTML templating library for Python 3',
 	long_description = "htmltemplate converts [X]HTML documents into simple template object models easily manipulated using ordinary Python code. It is powerful, flexible, and easy to use.",
 	author = 'HAS',
 	author_email = 'hhas@users.sourceforge.net',
-	url='https://bitbucket.org/hhas/htmltemplate',
+	url='https://github.com/hhas/htmltemplate',
 	py_modules = ['htmltemplate'],
 	classifiers = [
 		'Development Status :: 5 - Production/Stable',
